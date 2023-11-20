0f09b5e1612b99b3ad2237809b8cc0fd5c1d41e4
unknown <nikhilchakravarthy1303@gmail.com>
2023-11-20 21:44:01 +0530

    Initial Commit

---
+++ src/components/Interface.jsx
@@ -0,0 +1,359 @@
+import { ValidationError, useForm } from "@formspree/react";
+import { motion } from "framer-motion";
+import { useAtom } from "jotai";
+import { currentProjectAtom, projects } from "./Projects";
+import { FaLinkedin, FaInstagram, FaTwitter, FaGithub, FaRegAddressBook, FaRegFile } from "react-icons/fa";
+const Section = (props) => {
+  const { children, mobileTop } = props;
+
+  return (
+    <motion.section
+      className={`
+  h-screen w-screen p-8 max-w-screen-2xl mx-auto
+  flex flex-col items-start
+  ${mobileTop ? "justify-start md:justify-center" : "justify-center"}
+  `}
+      initial={{
+        opacity: 0,
+        y: 50,
+      }}
+      whileInView={{
+        opacity: 1,
+        y: 0,
+        transition: {
+          duration: 1,
+          delay: 0.6,
+        },
+      }}
+    >
+      {children}
+    </motion.section>
+  );
+};
+
+export const Interface = (props) => {
+  const { setSection } = props;
+  return (
+  
+    <div className="flex flex-col items-center w-screen">
+      <AboutSection setSection={setSection} />
+      <SkillsSection />
+      <ProjectsSection />
+      <ContactSection />
+    
+    </div>
+    
+ 
+  );
+};
+
+const AboutSection = (props) => {
+  const { setSection } = props;
+  return (
+    <Section mobileTop>
+      <h1 className="text-4xl text-black-800 md:text-6xl font-extrabold leading-snug mt-8 md:mt-0 italic">
+        Hi, I'm 
+        <br />
+        <span className=" text-black-800 ">Nikhil Chakravarthy</span>
+      </h1>
+      <motion.p
+        className="text-lg text-rose-2
+        00 mt-4"
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 1.5,
+        }}
+      >
+        I am a Software developer
+        <br />
+        
+      </motion.p>
+      <motion.button
+        onClick={() => setSection(3)}
+        className={`text-white py-4 px-8 
+      rounded-lg font-bold text-lg mt-4 md:mt-16`}
+      style={{ backgroundColor: '#ff923e' }}
+        initial={{
+          opacity: 0,
+          y: 25,
+        }}
+        whileInView={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1,
+          delay: 2,
+        }}
+      >
+        Get in touch
+      </motion.button>
+      
+    </Section>
+  );
+};
+
+const skills = [
+  {
+    title: "Java",
+    level: 80,
+  },
+  {
+    title: "React / React Native",
+    level: 90,
+  },
+  {
+    title: "Full Stack",
+    level: 90,
+  },
+  {
+    title: "MySQL",
+    level: 60,
+  },
+  {
+    title: "Ethical Hacking",
+    level: 40,
+  },
+  {
+    title: ".NET",
+    level: 30,
+  },
+];
+const languages = [
+  {
+    title: "Telugu",
+    level: 100,
+  },
+  {
+    title: "English",
+    level: 80,
+  },
+  {
+    title: "Hindi",
+    level: 50,
+  },
+];
+
+const SkillsSection = () => {
+  return (
+    <Section>
+      <motion.div className="w-full" whileInView={"visible"}>
+        <h2 className="text-3xl md:text-5xl font-bold text-white">Skills</h2>
+        <div className="mt-8 space-y-4">
+          {skills.map((skill, index) => (
+            <div className="w-full md:w-64" key={index}>
+              <motion.h3
+                className="text-lg md:text-xl font-bold text-gray-100"
+                initial={{
+                  opacity: 0,
+                }}
+                variants={{
+                  visible: {
+                    opacity: 1,
+                    transition: {
+                      duration: 1,
+                      delay: 1 + index * 0.2,
+                    },
+                  },
+                }}
+              >
+                {skill.title}
+              </motion.h3>
+              <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                <motion.div
+                  className="h-full bg-rose-500 rounded-full "
+                  style={{ width: `${skill.level}%` }}
+                  initial={{
+                    scaleX: 0,
+                    originX: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      scaleX: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 1 + index * 0.2,
+                      },
+                    },
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2 className="text-3xl md:text-5xl font-bold mt-10 text-white">
+            Languages
+          </h2>
+          <div className="mt-8 space-y-4">
+            {languages.map((lng, index) => (
+              <div className="w-full md:w-64" key={index}>
+                <motion.h3
+                  className="text-lg md:text-xl font-bold text-gray-100"
+                  initial={{
+                    opacity: 0,
+                  }}
+                  variants={{
+                    visible: {
+                      opacity: 1,
+                      transition: {
+                        duration: 1,
+                        delay: 2 + index * 0.2,
+                      },
+                    },
+                  }}
+                >
+                  {lng.title}
+                </motion.h3>
+                <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                  <motion.div
+                    className="h-full bg-rose-500 rounded-full "
+                    style={{ width: `${lng.level}%` }}
+                    initial={{
+                      scaleX: 0,
+                      originX: 0,
+                    }}
+                    variants={{
+                      visible: {
+                        scaleX: 1,
+                        transition: {
+                          duration: 1,
+                          delay: 2 + index * 0.2,
+                        },
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </motion.div>
+    </Section>
+  );
+};
+
+const ProjectsSection = () => {
+  const [currentProject, setCurrentProject] = useAtom(currentProjectAtom);
+
+  const nextProject = () => {
+    setCurrentProject((currentProject + 1) % projects.length);
+  };
+
+  const previousProject = () => {
+    setCurrentProject((currentProject - 1 + projects.length) % projects.length);
+  };
+
+  return (
+    <Section>
+      <div className="flex w-full h-full gap-8 items-center justify-center">
+        <button
+          className="hover:text-rose-600 transition-colors"
+          onClick={previousProject}
+        >
+          ← Previous
+        </button>
+        <h2 className="text-3xl md:text-5xl font-bold">Projects</h2>
+        <button
+          className="hover:text-rose-600 transition-colors"
+          onClick={nextProject}
+        >
+          Next →
+        </button>
+      </div>
+    </Section>
+  );
+};
+
+const ContactSection = () => {
+  const [state, handleSubmit] = useForm("xzblqypo");
+  const iconColor = "#e11d48";
+  return (
+    <Section>
+      <h2 className="text-3xl md:text-5xl font-bold">Contact me</h2>
+      <div className="mt-8 p-8 rounded-md bg-white bg-opacity-50 w-96 max-w-full">
+        {state.succeeded ? (
+          <p className="text-gray-900 text-center">Thanks for your message !</p>
+        ) : (
+          <form onSubmit={handleSubmit}>
+            <label for="name" className="font-medium text-gray-900 block mb-1">
+              Name
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-600 p-3"
+            />
+            <label
+              for="email"
+              className="font-medium text-gray-900 block mb-1 mt-8"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-600 p-3"
+            />
+            <ValidationError
+              className="mt-1 text-red-500"
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
+            <label
+              for="email"
+              className="font-medium text-gray-900 block mb-1 mt-8"
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              className="h-32 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-rose-600 p-3"
+            />
+            <ValidationError
+              className="mt-1 text-red-500"
+              errors={state.errors}
+            />
+            <button
+              disabled={state.submitting}
+              className="bg-orange-400 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16 "
+            >
+              Submit
+            </button>
+          </form>
+        )}
+      </div>
+      
+    <div className="flex space-x-2 mt-8">
+      <a href="https://www.linkedin.com/in/nikhil-chakravarthy-064504203/" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin size={30} color="#0076b5" />
+      </a>
+      <a href="https://instagram.com/nikhil.cha_thy" target="_blank" rel="noopener noreferrer">
+        <FaInstagram size={30} color={iconColor} />
+      </a>
+      <a href="https://github.com/Nikhilchakravarthy1303" target="_blank" rel="noopener noreferrer">
+        <FaGithub size={30} color="black" />
+      </a>
+      <a href="https://copper-trish-36.tiiny.site/" target="_blank" rel="noopener noreferrer">
+        <FaRegFile size={30} color="maroon" />
+        
+
+      </a>
+    </div>
+    </Section>
+    
+  );
+};
+
