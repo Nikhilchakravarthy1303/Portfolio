5242030e6a0cc9b8ed609928a427e736053f02e9
Nikhil Chakravarthy <75674351+Nikhilchakravarthy1303@users.noreply.github.com>
2023-12-31 11:44:17 +0530

    Update LoadingScreen.jsx

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -17,11 +17,10 @@ export const LoadingScreen = (props) => {
   return (
     <div
       className={`fixed top-0 left-0 w-full h-full z-50 transition-opacity duration-1000 pointer-events-none
-  flex items-center justify-center 
+  flex items-center justify-center bg-orange-50 
   ${started ? "opacity-0" : "opacity-100"}`}
-  
     >
-      <div className="text-4xl md:text-9xl font-bold  relative" style={{ color: '#ff923e' }}>
+      <div className="text-4xl md:text-9xl font-bold text-orange-400 relative">
         <div
           className="absolute left-0 top-0  overflow-hidden truncate text-clip transition-all duration-500"
           style={{
